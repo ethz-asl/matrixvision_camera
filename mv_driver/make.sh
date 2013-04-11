e42d46715effe1af6022c82e45ee5bf98ec00cf9
asl <asl@bluebird.(none)>
2013-04-11 17:34:38 +0200

    moved driver files to ethz webspace, fixed make.sh accordingly and fixed new library names fixes #4 closes #3

--- mv_driver/make.sh
+++ mv_driver/make.sh
@@ -6,15 +6,15 @@ PACKAGE_DIR=$(rospack find mv_driver)
 echo "### downloading and unpacking drivers to" $PACKAGE_DIR "###"
 
 
-BLUECOUGAR_URL="http://www.matrix-vision.com/GigE-Vision-camera-mvbluecougar-x.html?file=tl_files/mv11/support/mvIMPACT_Acquire/01"
-BLUEFOX_URL="http://www.matrix-vision.com/USB2.0-single-board-camera-mvbluefox-mlc.html?file=tl_files/mv11/support/mvIMPACT_Acquire/01"
+GENTL_URL="http://n.ethz.ch/~acmarkus/download/matrixvision_driver"
+BLUEFOX_URL="http://n.ethz.ch/~acmarkus/download/matrixvision_driver"
 
-BLUEFOX_FILE=""
-BLUECOUGAR_FILE=""
+#BLUEFOX_FILE=""
+#GENTL_FILE=""
 
 API=mvIMPACT_acquire
-VERSION=2.5.1
-BLUEFOX_VERSION=2.5.1
+VERSION=2.5.2
+BLUEFOX_VERSION=2.5.4
 ABI=ABI2
 
 LINKER_PATHS=$PACKAGE_DIR/linker_paths
@@ -28,63 +28,69 @@ else
     TARGET=x86_64
 fi
 
-BLUECOUGAR_NAME=mvGenTL_Acquire
-BLUECOUGAR_TARNAME=$BLUECOUGAR_NAME-$TARGET"_"$ABI-$VERSION
+GENTL_NAME=mvGenTL_Acquire
+GENTL_TARNAME=$GENTL_NAME-$TARGET"_"$ABI-$VERSION
 
 BLUEFOX_NAME=mvBlueFOX
 BLUEFOX_TARNAME=$BLUEFOX_NAME-$TARGET"_"$ABI-$BLUEFOX_VERSION
 
 
 # cleanup first
-rm -rf $BLUECOUGAR_NAME* $BLUEFOX_NAME* $API* tmp $LINKER_PATHS $COMPILER_FLAGS $ENV_VARS
+rm -rf $GENTL_NAME* $BLUEFOX_NAME* $API* tmp $LINKER_PATHS $COMPILER_FLAGS $ENV_VARS
 rm -rf download
 
 #### download driver archives ####
 mkdir -p download
 cd download
-wget -O $BLUECOUGAR_TARNAME.tgz -nc $BLUECOUGAR_URL/$BLUECOUGAR_TARNAME.tgz 
-wget -O $BLUEFOX_TARNAME.tgz -nc $BLUEFOX_URL/$BLUEFOX_TARNAME.tgz
+wget -O $GENTL_TARNAME.tar -nc $GENTL_URL/$GENTL_TARNAME.tar 
+wget -O $BLUEFOX_TARNAME.tar -nc $BLUEFOX_URL/$BLUEFOX_TARNAME.tar
 
 
 #### unpack blueCougar ####
 
-mkdir -p $PACKAGE_DIR/tmp/$BLUECOUGAR_NAME
-cd $PACKAGE_DIR/tmp/$BLUECOUGAR_NAME
-tar -xf $PACKAGE_DIR/download/$BLUECOUGAR_TARNAME.tgz --overwrite
+mkdir -p $PACKAGE_DIR/tmp/$GENTL_NAME
+cd $PACKAGE_DIR/tmp/$GENTL_NAME
+tar -xf $PACKAGE_DIR/download/$GENTL_TARNAME.tar --overwrite
 
 #### BlueCougar runtime stuff ####
 
 cd $PACKAGE_DIR
-mkdir -p $BLUECOUGAR_NAME"_runtime"
-cd $BLUECOUGAR_NAME"_runtime"
-tar xf $PACKAGE_DIR/tmp/$BLUECOUGAR_NAME/$BLUECOUGAR_NAME"_"runtime-$VERSION.tar --overwrite
+mkdir -p $GENTL_NAME"_runtime"
+cd $GENTL_NAME"_runtime"
+tar xf $PACKAGE_DIR/tmp/$GENTL_NAME/$GENTL_NAME"_"runtime-$VERSION.tar --overwrite
 
 cd lib
 # kick out devicemanager and prophandling, since this is not device specific
 rm -f libmvDeviceManager* 
-rm -f libMvPropHandling*
+rm -f libmvPropHandling*
+
+# those should not be necessary anymore
+#ln -fs libmvBlueCOUGAR.so.$VERSION libmvBlueCOUGAR.so
+#ln -fs libmvTLIClientGigE.so mvTLIClientGigE.cti
+#ln -fs libmvTLIClientGigE.so.$VERSION libmvTLIClientGigE.so
+rm -f libmvBlueCOUGAR*
+rm -f libmvTLIClientGigE*
+
 # create missing symlinks
-ln -fs libmvBlueCOUGAR.so.$VERSION libmvBlueCOUGAR.so
-ln -fs libmvTLIClientGigE.so.$VERSION libmvTLIClientGigE.so
-ln -fs libmvTLIClientGigE.so mvTLIClientGigE.cti
 ln -fs libmvGenTLConsumer.so.$VERSION libmvGenTLConsumer.so
 ln -fs libmvGenTLProducer.so.$VERSION libmvGenTLProducer.so
-ln -fs libmvPropHandling.so.$VERSION libmvPropHandling.so
-echo -L$PACKAGE_DIR/$BLUECOUGAR_NAME"_runtime/lib" >> $LINKER_PATHS
+ln -fs libmvGenTLProducer.so libmvGenTLProducer.cti
+#ln -fs libmvPropHandling.so.$VERSION libmvPropHandling.so
+echo -L$PACKAGE_DIR/$GENTL_NAME"_runtime/lib" >> $LINKER_PATHS
 
 #### mvImpact (device independent stuff) ####
 cd $PACKAGE_DIR
-tar xf $PACKAGE_DIR/tmp/$BLUECOUGAR_NAME/$API-$VERSION.tar --overwrite
+tar xf $PACKAGE_DIR/tmp/$GENTL_NAME/$API-$VERSION.tar --overwrite
 mv $API-$VERSION $API
 echo -L$PACKAGE_DIR/$API/lib/$TARGET >> $LINKER_PATHS
 echo -I$PACKAGE_DIR/$API >> $COMPILER_FLAGS
 
 #### GenICam runtime/compile time stuff ####
 
-cd $PACKAGE_DIR/$BLUECOUGAR_NAME"_runtime"
+cd $PACKAGE_DIR/$GENTL_NAME"_runtime"
 mkdir -p GenICam
 cd GenICam
-# the runtime tar contains either the i86 or the x64 tgz
+# the runtime tar contains either the i86 or the x64 tar
 if [ -r ../GenICam_Runtime_gcc40_Linux32_i86_v*.tgz ]; then
        
    if [ x$TARGET != xx86 ]; then
@@ -92,8 +98,8 @@ if [ -r ../GenICam_Runtime_gcc40_Linux32_i86_v*.tgz ]; then
       exit 1
    fi
    tar xfz ../GenICam_Runtime_gcc40_Linux32_i86_v*.tgz;
-   echo -L$PACKAGE_DIR/$BLUECOUGAR_NAME"_runtime"/GenICam/bin/Linux32_i86 >> $LINKER_PATHS
-   echo -L$PACKAGE_DIR/$BLUECOUGAR_NAME"_runtime"/GenICam/bin/Linux32_i86/GenApi/Generic >> $LINKER_PATHS
+   echo -L$PACKAGE_DIR/$GENTL_NAME"_runtime"/GenICam/bin/Linux32_i86 >> $LINKER_PATHS
+   echo -L$PACKAGE_DIR/$GENTL_NAME"_runtime"/GenICam/bin/Linux32_i86/GenApi/Generic >> $LINKER_PATHS
 fi
 if [ -r ../GenICam_Runtime_gcc40_Linux64_x64_v*.tgz ]; then
    if [ x$TARGET = xx86 ]; then
@@ -101,27 +107,27 @@ if [ -r ../GenICam_Runtime_gcc40_Linux64_x64_v*.tgz ]; then
       exit 1
    fi
    tar xfz ../GenICam_Runtime_gcc40_Linux64_x64_v*.tgz;
-   echo -L$PACKAGE_DIR/$BLUECOUGAR_NAME"_runtime"/GenICam/bin/Linux64_x64 >> $LINKER_PATHS
-   echo -L$PACKAGE_DIR/$BLUECOUGAR_NAME"_runtime"/GenICam/bin/Linux64_x64/GenApi/Generic >> $LINKER_PATHS
+   echo -L$PACKAGE_DIR/$GENTL_NAME"_runtime"/GenICam/bin/Linux64_x64 >> $LINKER_PATHS
+   echo -L$PACKAGE_DIR/$GENTL_NAME"_runtime"/GenICam/bin/Linux64_x64/GenApi/Generic >> $LINKER_PATHS
 fi
 
 # pass the genicam paths to the compiler, such that the app can setup the environment itself
-echo -DGENICAM_ROOT=$PACKAGE_DIR/$BLUECOUGAR_NAME"_runtime"/GenICam >> $COMPILER_FLAGS
-echo -DGENICAM_ROOT_V2_3=$PACKAGE_DIR/$BLUECOUGAR_NAME"_runtime"/GenICam >> $COMPILER_FLAGS
-echo -DGENICAM_GENTL64_PATH=$PACKAGE_DIR/$BLUECOUGAR_NAME"_runtime"/lib >> $COMPILER_FLAGS
-echo -DGENICAM_LOG_CONFIG_V2_3=$PACKAGE_DIR/$BLUECOUGAR_NAME"_runtime"/GenICam/log/config-unix/DefaultLogging.properties >> $COMPILER_FLAGS
+echo -DGENICAM_ROOT=$PACKAGE_DIR/$GENTL_NAME"_runtime"/GenICam >> $COMPILER_FLAGS
+echo -DGENICAM_ROOT_V2_3=$PACKAGE_DIR/$GENTL_NAME"_runtime"/GenICam >> $COMPILER_FLAGS
+echo -DGENICAM_GENTL64_PATH=$PACKAGE_DIR/$GENTL_NAME"_runtime"/lib >> $COMPILER_FLAGS
+echo -DGENICAM_LOG_CONFIG_V2_3=$PACKAGE_DIR/$GENTL_NAME"_runtime"/GenICam/log/config-unix/DefaultLogging.properties >> $COMPILER_FLAGS
 
-echo export GENICAM_ROOT=$PACKAGE_DIR/$BLUECOUGAR_NAME"_runtime"/GenICam >> $ENV_VARS
-echo export GENICAM_ROOT_V2_3=$PACKAGE_DIR/$BLUECOUGAR_NAME"_runtime"/GenICam >> $ENV_VARS
-echo export GENICAM_GENTL64_PATH=$PACKAGE_DIR/$BLUECOUGAR_NAME"_runtime"/lib >> $ENV_VARS
-echo export GENICAM_LOG_CONFIG_V2_3=$PACKAGE_DIR/$BLUECOUGAR_NAME"_runtime"/GenICam/log/config-unix/DefaultLogging.properties >> $ENV_VARS
+echo export GENICAM_ROOT=$PACKAGE_DIR/$GENTL_NAME"_runtime"/GenICam >> $ENV_VARS
+echo export GENICAM_ROOT_V2_3=$PACKAGE_DIR/$GENTL_NAME"_runtime"/GenICam >> $ENV_VARS
+echo export GENICAM_GENTL64_PATH=$PACKAGE_DIR/$GENTL_NAME"_runtime"/lib >> $ENV_VARS
+echo export GENICAM_LOG_CONFIG_V2_3=$PACKAGE_DIR/$GENTL_NAME"_runtime"/GenICam/log/config-unix/DefaultLogging.properties >> $ENV_VARS
 
 
 #### bluefox runtime ####
 # unpack
 mkdir -p $PACKAGE_DIR/tmp/$BLUEFOX_NAME
 cd $PACKAGE_DIR/tmp/$BLUEFOX_NAME
-tar -xf $PACKAGE_DIR/download/$BLUEFOX_TARNAME.tgz --overwrite
+tar -xf $PACKAGE_DIR/download/$BLUEFOX_TARNAME.tar --overwrite
 
 # copy blueFOX runtime libs
 mkdir -p $PACKAGE_DIR/$BLUEFOX_NAME"_runtime/lib/"$TARGET
